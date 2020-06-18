51148be772651ffca9368ed900bfcc45fb7c9cf1
Matheus <matheus.barboza.henrique@gmail.com>
2020-06-18 12:09:53 -0300

    inicio

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExercicoISESI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
